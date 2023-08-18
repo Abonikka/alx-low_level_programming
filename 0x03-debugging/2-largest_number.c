2f53eb113d3e45dafbe223657fd1597e952185f8
Abonikka94 <donchinazanaz@gmail.com>
2023-08-18 15:38:49 -0700

    more funct

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -7,18 +7,14 @@
  * @c: third integer
  * Return: largest number
  */
-int largest_number(int a, int b, int c)
 
+int largest_number(int a, int b, int c)
 {
 	int largest;
 
 	if (a > b && a > c)
 	{
 		largest = a;
-	}
-	else if (a > b && c > a)
-	{
-		largest = c;
 	}
 	else if (b > c)
 	{
@@ -28,6 +24,6 @@ int largest_number(int a, int b, int c)
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
-

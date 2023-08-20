8d43371e714ae73eeed3cdfa8be5a7ea78db2e7c
Abonikka94 <donchinazanaz@gmail.com>
2023-08-19 17:58:30 -0700

    world hello

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
-#include<stdio.h>
-/**
- * main - Entry point
- * written by chinagorm
- * Return: Always 0 (Success)
- */
+#include <stdio.h>
 
+/**
+ * main - Prints "Programming is like building a multilingual
+ *                puzzle, followed by a new line.
+ * written by chinagorom
+ * Return: Always 0.
+ */
 int main(void)
 {
-	puts("\" programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }

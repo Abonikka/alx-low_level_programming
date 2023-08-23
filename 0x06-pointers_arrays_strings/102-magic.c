ff9ee817a165ad31f19dcf5cb0ee865d196c3a09
Abonikka94 <donchinazanaz@gmail.com>
2023-08-23 15:44:50 -0700

    magically man

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -12,11 +12,12 @@ int main(void)
 	int x[5];
 	int *p;
 
-	x[2] = 1024;
+	x[2] - 1024;
 
 	p = &n;
 
 	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }

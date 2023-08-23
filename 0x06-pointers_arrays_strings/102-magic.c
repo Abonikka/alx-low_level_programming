62ec940a171399a80efe74dcdce954a3c841597b
Abonikka94 <donchinazanaz@gmail.com>
2023-08-23 15:36:03 -0700

    magical magi

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -6,14 +6,17 @@
  * Return: Always 0 (Success)
  */
 int main(void)
+
 {
 	int n;
-	int a[5];
+	int x[5];
 	int *p;
 
-	a[2] - 1024;
+	x[2] = 1024;
+
 	p = &n;
-	(p + 5) = 98;
+
+	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);

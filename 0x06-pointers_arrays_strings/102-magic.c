f69c5b358eecd7b53d013722e8745d08471f49c4
Abonikka94 <donchinazanaz@gmail.com>
2023-08-23 16:38:55 -0700

    magic

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -16,14 +16,15 @@ int main(void)
 
 	x[2] - 1024;
 	p = &n;
+	printf("n = %p\n", &n);
+        printf("x[0] = %p\n", &x[0]);
+        printf("x[1] = %p\n", &x[1]);
+        printf("x[2] = %p\n", &x[2]);
+        printf("p = %p\n", (p + 5));
+        printf("x[3] = %p\n", &x[3]);
+        printf("a[2] = %d\n", x[2]);
 	(p + 5) = 98;
 	/* ...so that this prints 98\n */
-	printf("n = %p\n", &n);
-	printf("x[0] = %p\n", &x[0]);
-	printf("x[1] = %p\n", &x[1]);
-	printf("x[2] = %p\n", &x[2]);
-	printf("p = %p\n", (p + 5));
-	printf("x[3] = %p\n", &x[3]);
 	printf("a[2] = %d\n", x[2]);
 	return (0);
 }
